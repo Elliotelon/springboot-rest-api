e1d13fb6c4dbcbd8aabd09e4948e84b3b65e37fe
minkyu <elliotelon@gmail.com>
2022-12-23 12:54:12 +0900

    GetMapping 3 return List

--- src/main/java/com/elliot/springbootrestapi/controller/StudentController.java
+++ src/main/java/com/elliot/springbootrestapi/controller/StudentController.java
@@ -5,14 +5,28 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.elliot.springbootrestapi.bean.Student;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class StudentController {
     
     // http://localhost:8080/student
-
     @GetMapping("/student")
     public Student getStudent(){
         Student student = new Student(1, "minkyu", "kim");
         return student;
+    }
+
+    // http://localhost:8080/students
+    @GetMapping("/students")
+    public List<Student> getStudents(){
+        List<Student> students = new ArrayList<>();
+        students.add(new Student(1, "minkyu", "kim"));
+        students.add(new Student(2, "minkyu2", "kim"));
+        students.add(new Student(3, "minkyu3", "kim"));
+        students.add(new Student(4, "minkyu4", "kim"));
+
+        return students;
     }
 }

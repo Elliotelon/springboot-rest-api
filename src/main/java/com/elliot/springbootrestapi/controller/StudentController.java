af5e4e4d414234059c581df356b9b44794a32926
minkyu <elliotelon@gmail.com>
2022-12-23 14:13:44 +0900

    RequestParam 어노테이션

--- src/main/java/com/elliot/springbootrestapi/controller/StudentController.java
+++ src/main/java/com/elliot/springbootrestapi/controller/StudentController.java
@@ -3,6 +3,7 @@ package com.elliot.springbootrestapi.controller;
 import jakarta.annotation.Nullable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.elliot.springbootrestapi.bean.Student;
@@ -40,5 +41,14 @@ public class StudentController {
                                        @PathVariable("first-name") String firstName,
                                        @PathVariable("last-name") String lastName) {
         return new Student(studentId, firstName, lastName);
+    }
+
+    // RequestParam
+    // http://localhost:8080/students/query?id=1&firstName=minkyu&lastName=kim
+    @GetMapping("/students/query")
+    public Student studentRequestVariable(@RequestParam int id,
+                                          @RequestParam String firstName,
+                                          @RequestParam String lastName){
+        return new Student(id, firstName, lastName);
     }
 }
